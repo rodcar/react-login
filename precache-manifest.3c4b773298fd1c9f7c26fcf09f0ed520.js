f3c7e2ebbad897e794b4af1f8a5b52d64f45eb0c
rodcar <ivanrod12@hotmail.com>
2019-10-03 12:12:01 -0500

    Commit inicial

---
+++ precache-manifest.3c4b773298fd1c9f7c26fcf09f0ed520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e8e16f0a79cacc130a9a0590972132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e75262f790f5ee3af894",
+    "url": "/static/css/main.52c5b7a3.chunk.css"
+  },
+  {
+    "revision": "54c1b0ff9dceefcd035f",
+    "url": "/static/js/2.8e934a17.chunk.js"
+  },
+  {
+    "revision": "e75262f790f5ee3af894",
+    "url": "/static/js/main.118f10ba.chunk.js"
+  },
+  {
+    "revision": "f22f223b313dedaff74e",
+    "url": "/static/js/runtime-main.3083bb55.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
