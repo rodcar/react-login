df8ac2afe37932a4562334ad38916d6f45dcb81c
rodcar <ivanrod12@hotmail.com>
2019-10-03 12:28:02 -0500

    Updates

---
+++ precache-manifest.f72f5a90618172ba7570a0931b2f1afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b71d46434a5ee83c185c22098f9ada",
+    "url": "/react-login/index.html"
+  },
+  {
+    "revision": "06e4f7bfc460c0c958eb",
+    "url": "/react-login/static/css/main.52c5b7a3.chunk.css"
+  },
+  {
+    "revision": "54c1b0ff9dceefcd035f",
+    "url": "/react-login/static/js/2.8e934a17.chunk.js"
+  },
+  {
+    "revision": "06e4f7bfc460c0c958eb",
+    "url": "/react-login/static/js/main.4f6417bf.chunk.js"
+  },
+  {
+    "revision": "1d0fcb2c1c19f5bb568a",
+    "url": "/react-login/static/js/runtime-main.fabd3c6c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-login/static/media/logo.25bf045c.svg"
+  }
+]);
